3920026233c50b90ffebde74d38bcad6c05a69df
lokeshdj24 <lokesh@digitaljalebilabs.com>
2025-04-22 17:42:01 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: resolve(__dirname, "./index.jsx"),
       name: "venti-ui-kit",
       fileName: (format) => `index.${format}.js`,
-      formats: ["esm"],
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
