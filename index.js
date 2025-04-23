7190eec9c4c9c2a9263434d974512197055668dd
lokeshdj24 <lokesh@digitaljalebilabs.com>
2025-04-23 10:28:40 +0530

    jdk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export { Button } from "./src/components/button/Button";
-export { Heading } from "./src/components/heading1/Heading1";
-export { Body } from "./src/components/body/Body";
-export { Caption } from "./src/components/caption/Caption";
-export { Surface } from "./src/components/surface/Surface";
+export { Button } from "./src/components/button/Button.jsx";
+export { Heading } from "./src/components/heading1/Heading1.jsx";
+export { Body } from "./src/components/body/Body.jsx";
+export { Caption } from "./src/components/caption/Caption.jsx";
+export { Surface } from "./src/components/surface/Surface.jsx";
